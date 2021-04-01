062d1ada6a90540801c26e11ed4dd3fcf22dd54f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-03-31 22:12:53 -0600

    Versión 2: planificación y configuración inicial

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/post', App\Http\Controllers\Api\V1\PostController::class)
+Route::apiResource('v1/post', PostV1::class)
     ->only(['index', 'show', 'destroy']);
+
+
+Route::apiResource('v2/post', PostV2::class)
+    ->only(['index', 'show']);
