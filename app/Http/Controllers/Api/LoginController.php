dffbce420de28b72b2e217f01a71a453460b1ead
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-04-01 19:07:39 -0600

    Acceso

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -13,14 +13,23 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $this->validateLogin($request);
-        //login true
 
-        //login false
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
+
     public function validateLogin(Request $request)
     {
         return $request->validate([
-            'email' => 'required!email',
+            'email' => 'required|email',
             'password' => 'required',
             'name' => 'required'
         ]);
